5230fd8945dd586e16f33edb09d29590f6958206
das0166 <das0166@naver.com>
2022-03-10 01:09:55 +0900

    [2022.03.09]Green복지재단_연습

--- 2.Green복지재단/js/custion.js
+++ 2.Green복지재단/js/custion.js
@@ -17,4 +17,18 @@ $(document).ready(function(){
     $(".slide li").eq(SlideI).siblings().animate({left:"-1200px"},500);
     $(".slide li").eq(SlideI).animate({left:"0"},500);
     },3000);
+
+    //패밀리 사이트
+    $("footer .fam p").click(function(){
+        $(this).toggleClass("on");
+        $("footer .fam ul").stop().slideToggle();
+    });
+
+    //공지사항 첫번째 모달
+    $(".contents .notice ul li").eq(0).click(function(){
+        $(".modal").fadeIn();
+    });
+    $(".btn").click(function(){
+        $(".modal").fadeOut();
+    });
 }); //ready end
