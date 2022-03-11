09ecdfec8310e441e47e77f6adfd0a6754a37e5c
das0166 <das0166@naver.com>
2022-03-12 02:30:49 +0900

    [2022.03.11]슬라이드 css, js까지 완성

--- 3.강원천문대/js/script.js
+++ 3.강원천문대/js/script.js
@@ -5,4 +5,17 @@ $(document).ready(function(){
     },function(){
         $(this).find(".sub").stop().slideUp(); 
     });
+
+    //슬라이더
+    $(".slide .slide_pi>li").eq(0).siblings().hide();
+    var slideI=0;
+    setInterval(function(){
+        if(slideI<2){
+            slideI++;
+        }else{
+            slideI=0;
+        }
+    $(".slide .slide_pi>li").eq(slideI).siblings().fadeOut(500);
+    $(".slide .slide_pi>li").eq(slideI).fadeIn(500);
+    },3000);
 });
