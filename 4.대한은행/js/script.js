ae7233459265e4879216da8bd31df07729970f39
das0166 <das0166@naver.com>
2022-03-14 00:21:01 +0900

    [2022.03.13]대한은행_연습

--- 4.대한은행/js/script.js
+++ 4.대한은행/js/script.js
@@ -4,5 +4,33 @@ $(document).ready(function(){
         $(".sub").stop().slideDown();
        }, function(){$(".sub").stop().slideUp();
     });
+
+    //슬라이더
+    $(".slide .slide_all>li").eq(0).siblings().css({"left":"-1200px"});
+    var slideI=0;
+    setInterval(function(){
+        if(slideI<2){
+            slideI++;
+        }else{
+            slideI=0;
+        }
+    $(".slide .slide_all>li").eq(slideI).siblings().animate({"left":"-1200px"},500)
+    $(".slide .slide_all>li").eq(slideI).animate({"left":"0"},500)
+    },3000);
+    
+    //메뉴
+    $(".contents .notice_gal h2").click(function(){
+        $(".contents .notice_gal h2, .contents .notice_gal ul").removeClass("on");
+        $(this).addClass("on");
+        $(this).next("ul").addClass("on");
+    });
+
+    //공지사항 첫번째
+    $(".contents .notice_gal .notice>li>a").eq(0).click(function(){
+        $(".modal").fadeIn();
+    });
+   $(".btn").click(function(){
+        $(".modal").fadeOut();
+   });
 });
     
