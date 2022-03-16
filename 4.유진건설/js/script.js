f23add796851608901ba575ad2b67b1ad492e8e1
das0166 <das0166@naver.com>
2022-03-16 21:41:21 +0900

    [2022.03.16] footer css 완성 전, footer 전까지 완성

--- 4.유진건설/js/script.js
+++ 4.유진건설/js/script.js
@@ -6,4 +6,16 @@ $(document).ready(function(){
    $("header nav .gnb>li").mouseleave(function(){
     $(".nav_bg, .sub").stop().slideUp();
    });
+   // 스크립트
+   $(".slide li").eq(0).siblings().hide();
+   var slideI=0;
+   setInterval(function(){
+        if(slideI<2){
+            slideI++;
+        }else{
+            slideI=0;
+        }
+    $(".slide li").eq(slideI).siblings().fadeOut(500);
+    $(".slide li").eq(slideI).fadeIn(500);
+   },3000);
 });
