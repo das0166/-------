06ef73aa64872180e4d431be79972b543ec534f1
das0166 <das0166@naver.com>
2022-03-17 02:29:45 +0900

    [2022.03.16]유진건설_연습

--- 4.유진건설/js/script.js
+++ 4.유진건설/js/script.js
@@ -18,4 +18,15 @@ $(document).ready(function(){
     $(".slide li").eq(slideI).siblings().fadeOut(500);
     $(".slide li").eq(slideI).fadeIn(500);
    },3000);
+   //패밀리사이트
+   $("footer .fam p").click(function(){
+        $("footer .fam ul").slideToggle();
+   });
+   //모달
+   $(".contents .notice ul li").eq(0).click(function(){
+        $(".modal").fadeIn();
+   });
+   $(".btn").click(function(){
+        $(".modal").fadeOut();
+   });
 });
