e2a368083ed6323b90eb4a3da730fe2c05a65331
das0166 <das0166@naver.com>
2022-03-20 19:02:29 +0900

    [2022.03.20]해운대빛축제_연습

--- 9.해운대 빛축제/js/script.js
+++ 9.해운대 빛축제/js/script.js
@@ -15,4 +15,15 @@ $(document).ready(function(){
         $(".container .slide li").eq(slideI).siblings().fadeOut();
         $(".container .slide li").eq(slideI).fadeIn();
     },3000);
+    $(".contents .no_gal h2").click(function(){
+        $(".contents .no_gal h2, .contents .no_gal ul").removeClass("on");
+        $(this).addClass("on");
+        $(this).next("ul").addClass("on");
+    });
+    $(".contents .no_gal .notice>li").eq(0).click(function(){
+        $(".modal").fadeIn();
+    });
+    $(".btn").click(function(){
+        $(".modal").fadeOut();
+    });
 });
