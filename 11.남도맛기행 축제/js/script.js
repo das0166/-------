92f17bffa58a66e0ca704864559a7d8d84612db0
das0166 <das0166@naver.com>
2022-03-23 23:24:30 +0900

    [2022.03.23]슬라이더 완성

--- 11.남도맛기행 축제/js/script.js
+++ 11.남도맛기행 축제/js/script.js
@@ -4,4 +4,15 @@ $(document).ready(function(){
     },function(){
         $(this).find(".sub").stop().slideUp(300);
     });
+    $(".slide>.slide_all>li").eq(0).siblings().css({"left":"-800px"});
+    var slideI=0;
+    setInterval(function(){
+        if(slideI<2){
+            slideI++;
+        }else{
+            slideI=0;
+        }
+        $(".slide>.slide_all>li").eq(slideI).siblings().animate({"left":"-800px"},500);
+        $(".slide>.slide_all>li").eq(slideI).animate({"left":"0"},500);
+    },3000)
 });
