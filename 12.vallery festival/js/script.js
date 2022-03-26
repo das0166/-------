f7bb91050d0fdbba76e5bf46c382599a41a5a9a8
das0166 <das0166@naver.com>
2022-03-27 00:55:24 +0900

    [2022.03.27]valleryfestival_연습

--- 12.vallery festival/js/script.js
+++ 12.vallery festival/js/script.js
@@ -15,4 +15,13 @@ $(document).ready(function(){
         $(".slide .slide_all>li").eq(slideI).siblings().animate({"left":"-800px"},500);
         $(".slide .slide_all>li").eq(slideI).animate({"left":"0"},500);
     },3000);
+    $("footer .fa h2").click(function(){
+        $("footer .fa ul").stop().slideToggle();
+    });
+    $(".contents .notice>ul>li").eq(0).click(function(){
+        $(".modal").fadeIn();
+    });
+    $(".btn").click(function(){
+        $(".modal").fadeOut();
+    });
 });
