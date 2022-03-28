1cd895fcea3a0f819a5472197e2909c094a3d9d1
das0166 <das0166@naver.com>
2022-03-28 23:50:31 +0900

    [2022.03.28]JUST쇼핑몰 콘텐츠까지 완성

--- 1.JUST쇼핑몰/시험/js/script.js
+++ 1.JUST쇼핑몰/시험/js/script.js
@@ -4,4 +4,20 @@ $(document).ready(function(){
     },function(){
         $(this).find(".sub").stop().slideUp();
     });
+   $(".slide .slide_all li").eq(0).siblings().css({"top":"-300px"});
+   var slideI=0;
+   setInterval(function(){
+        if(slideI<2){
+            slideI++;
+        }else{
+            slideI=0;
+        }
+        $(".slide .slide_all li").eq(slideI).siblings().animate({"top":"-300px"},500);
+        $(".slide .slide_all li").eq(slideI).animate({"top":"0"},500);
+    },3000);
+   $(".contents .no_gal h2").click(function(){
+        $(".contents .no_gal h2, .contents .no_gal ul").removeClass("on");
+        $(this).addClass("on");
+        $(this).next("ul").addClass("on");
+   });
 });
