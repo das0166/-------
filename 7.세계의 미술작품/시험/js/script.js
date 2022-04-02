0d4adba4d68f7adc7d1ae1ded0e182389cce4154
das0166 <das0166@naver.com>
2022-04-02 19:02:16 +0900

    [2022.04.02]세계의미술작품_완성

---
+++ 7.세계의 미술작품/시험/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $("header nav .gnb>li").hover(function(){
+        $(this).find(".sub").stop().slideDown();
+    },function(){
+        $(this).find(".sub").stop().slideUp();
+    });
+    $(".slide .slide_all>li").eq(0).siblings().css({"top":"-300px"});
+    var slideI=0;
+    setInterval(function(){
+        if(slideI<2){
+            slideI++;
+        }else{
+            slideI=0;
+        }
+        $(".slide .slide_all>li").eq(slideI).siblings().animate({"top":"-300px"},500);
+        $(".slide .slide_all>li").eq(slideI).animate({"top":"0"},500);
+    },3000);
+    $(".contents .notice ul>li").eq(0).click(function(){
+        $(".modal").fadeIn();
+    });
+    $(".btn").click(function(){
+        $(".modal").fadeOut();
+    })
+});
